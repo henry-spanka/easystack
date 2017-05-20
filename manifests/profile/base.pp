279885d96110fc191aa9c477303ae6199a30549d
Henry Spanka <henry@myvirtualserver.de>
2017-05-20 17:44:29 +0200

    setup openstack respository and install openstackclient and selinux

--- manifests/profile/base.pp
+++ manifests/profile/base.pp
@@ -2,4 +2,33 @@
 class easystack::profile::base {
     # make sure the parameters are initialized
     include ::easystack
+
+    # Setup OpenStack repository on all nodes
+    class { '::openstack_extras::repo::redhat::redhat':
+        gpgkey_hash => {
+            '/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-SIG-Cloud' => {
+                source => 'puppet:///modules/openstack_extras/RPM-GPG-KEY-CentOS-SIG-Cloud'
+            }
+        },
+        manage_epel => false,
+        manage_rdo  => false,
+        manage_virt => false,
+        repo_hash   => {
+            'CentOS-OpenStack-Octata' => {
+                descr   => 'CentOS-$releasever - Openstack Octata',
+                baseurl => 'http://mirror.centos.org/centos/$releasever/cloud/$basearch/openstack-ocata/',
+                gpgkey  => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-SIG-Cloud'
+            }
+        }
+    }
+
+    package { 'python-openstackclient':
+        ensure => installed,
+        name   => 'python-openstackclient'
+    }
+
+    package { 'openstack-selinux':
+        ensure => installed,
+        name   => 'openstack-selinux'
+    }
 }
