867424939c07d322e15ae315b6046616130e202d
Henry Spanka <henry@myvirtualserver.de>
2017-05-20 16:08:06 +0200

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,47 @@
+# Class: easystack
+# ===========================
+#
+# Manage your OpenStack environment using Puppet and easystack with ease.
+#
+# Parameters
+# ----------
+#
+# Document parameters here.
+#
+# * `sample parameter`
+# Explanation of what this parameter affects and what it defaults to.
+# e.g. "Specify one or more upstream ntp servers as an array."
+#
+# Variables
+# ----------
+#
+# Here you should define a list of variables that this module would require.
+#
+# * `sample variable`
+#  Explanation of how this variable affects the function of this class and if
+#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#  External Node Classifier as a comma separated list of hostnames." (Note,
+#  global variables should be avoided in favor of class parameters as
+#  of Puppet 2.6.)
+#
+# Examples
+# --------
+#
+# @example
+#    class { 'easystack':
+#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#    }
+#
+# Authors
+# -------
+#
+# Henry Spanka <henry@myvirtualserver.de>
+#
+# Copyright
+# ---------
+#
+# Copyright 2017 Henry Spanka, unless otherwise noted.
+#
+class easystack {
+
+}
