1f87709ca03ed4e1ebf1af39e88bab296207585c
Henry Spanka <henry@myvirtualserver.de>
2017-05-26 18:01:54 +0200

    setup chrony and memcached on HA controllers

---
+++ manifests/role/ha/controller/slave.pp
@@ -0,0 +1,21 @@
+# The role for the OpenStack slave controller(s)
+class easystack::role::ha::controller::slave inherits ::easystack::role {
+    # Make sure the time is synced on the controller nodes
+    class { 'chrony':
+        pool_use => false,
+        servers  => [
+            '0.pool.ntp.org',
+            '1.pool.ntp.org',
+            '2.pool.ntp.org',
+            '3.pool.ntp.org',
+        ],
+    }
+
+    $management_ip = ip_for_network($::easystack::config::management_network)
+
+    # Install and configure Memcached
+    class { 'memcached':
+        listen_ip  => "127.0.0.1,::1,${management_ip}",
+        max_memory => '20%',
+    }
+}
