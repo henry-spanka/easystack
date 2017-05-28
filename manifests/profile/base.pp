d18051e4c364275f38a3162d7502bcf5181658e5
Henry Spanka <henry@myvirtualserver.de>
2017-05-28 14:06:20 +0200

    remove openstack_extras module dependency

--- manifests/profile/base.pp
+++ manifests/profile/base.pp
@@ -4,22 +4,18 @@ class easystack::profile::base {
     include ::easystack
 
     # Setup OpenStack repository on all nodes
-    class { '::openstack_extras::repo::redhat::redhat':
-        gpgkey_hash => {
-            '/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-SIG-Cloud' => {
-                source => 'puppet:///modules/openstack_extras/RPM-GPG-KEY-CentOS-SIG-Cloud'
-            }
-        },
-        manage_epel => false,
-        manage_rdo  => false,
-        manage_virt => false,
-        repo_hash   => {
-            'CentOS-OpenStack-Octata' => {
-                descr   => 'CentOS-$releasever - Openstack Octata',
-                baseurl => 'http://mirror.centos.org/centos/$releasever/cloud/$basearch/openstack-ocata/',
-                gpgkey  => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-SIG-Cloud'
-            }
-        }
+    file { '/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-SIG-Cloud':
+        ensure => present,
+        source => 'puppet:///modules/easystack/RPM-GPG-KEY-CentOS-SIG-Virtualization',
+    }
+
+    yumrepo { 'CentOS-OpenStack-Octata':
+        baseurl  => 'ttp://mirror.centos.org/centos/$releasever/cloud/$basearch/openstack-ocata/',
+        descr    => 'CentOS-$releasever - Openstack Octata',
+        enabled  => 1,
+        gpgcheck => 1,
+        gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-SIG-Cloud',
+        require  => File['/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-SIG-Cloud'],
     }
 
     package { 'python-openstackclient':
