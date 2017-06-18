a0ebe0855f673126766a1ac753f8404580ec5732
Henry Spanka <henry@myvirtualserver.de>
2017-06-18 13:49:42 +0200

    configure neutron metadata proxy in nova

--- manifests/profile/nova/neutron.pp
+++ manifests/profile/nova/neutron.pp
@@ -3,6 +3,7 @@ class easystack::profile::nova::neutron (
     String $vip              = $::easystack::config::controller_vip,
     String $neutron_password = $::easystack::config::keystone_neutron_password,
     String $region           = $::easystack::config::keystone_region,
+    String $shared_secret    = $::easystack::config::neutron_metadata_shared_secret,
 ) {
     # make sure the parameters are initialized
     include ::easystack
@@ -20,5 +21,12 @@ class easystack::profile::nova::neutron (
         firewall_driver             => 'nova.virt.firewall.NoopFirewallDriver',
         neutron_region_name         => $region,
     }
+
+    # lint:ignore:duplicate_params
+    nova_config {
+        'neutron/service_metadata_proxy': value => true;
+        'neutron/metadata_proxy_shared_secret': value => $shared_secret;
+    }
+    # lint:endignore
 
 }
