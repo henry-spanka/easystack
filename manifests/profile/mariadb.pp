d19ac1c705452920ac7d021ea2eab6845192361a
Henry Spanka <henry@myvirtualserver.de>
2017-10-06 21:50:14 +0200

    do not notify mysqld after setting systemd limits

--- manifests/profile/mariadb.pp
+++ manifests/profile/mariadb.pp
@@ -90,7 +90,6 @@ class easystack::profile::mariadb (
         path        => ['/usr/bin', '/bin', '/usr/sbin'],
         refreshonly => true,
         before      => Class['mysql::server::installdb'],
-        notify      => Service['mysqld'],
     }
 
     # See: https://jira.mariadb.org/browse/MDEV-10767
